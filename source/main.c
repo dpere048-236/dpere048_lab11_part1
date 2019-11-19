b41a187a530c3af0db310496d5b1d7e6eaa2fe6b
David Perez <dpere048@wch136-05.cs.ucr.edu>
2019-11-18 18:00:17 -0800

    included

--- source/main.c
+++ source/main.c
@@ -1,16 +1,23 @@
-/*	Author: dpere048
+/*	Author:David Perez dpere048
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #11  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
- *
+ 
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#endif
+#include <avr/interrupt.h>
+#include "bit.h"
+#include "timer.h"
+#include "io.c"
+#include "io.h"
+#include "keypad.h"
+#include "scheduler.h"
+#include <stdio.h>
 
 int main(void) {
     /* Insert DDR and PORT initializations */
